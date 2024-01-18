cc71959db69198eebe53240bffc468bd0d851273
arsh-gupta-8 <SlurpIt.MBMS@gmail.com>
2024-01-18 22:05:59 +0700

    Added background, cookie a counter which adds a cookie every click

--- main.py
+++ main.py
@@ -4,42 +4,53 @@ pygame.init()
 
 # Text
 pygame.font.init()
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
-text_surface = my_font.render('Some Text', False, (0, 0, 0))
+font = pygame.font.SysFont('Comic Sans MS', 30)
 
 # Screen
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((1000, 500))
 pygame.display.set_caption("Cookie Clicker")
+cookie_img = pygame.image.load("cookie.png")
+cookie = pygame.transform.scale(cookie_img, (250, 250))
+cookie2 = pygame.transform.scale(cookie_img, (300, 300))
 
 # Loop Start
 clock = pygame.time.Clock()
 running = True
-FPS = 60
+FPS = 30
 
 # Game Setting
 cookies = 0
-
+enlarge = False
 
 def write(text):
-    write_line = my_font.render(text, False, (0, 0, 0))
+    write_line = font.render("Cookies: " + str(text), False, (0, 0, 0))
     screen.blit(write_line, (0, 0))
 
 
 while running:
-    clock.tick(FPS)
-    pygame.display.update()
-    write(cookies)
+
+    x, y = pygame.mouse.get_pos()
+    if 75 <= x <= 325 and 90 <= y <= 340:
+        enlarge = True
+    else:
+        enlarge = False
 
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1 and 75 <= x <= 325 and 90 <= y <= 340:
                 cookies += 1
 
-        if event.type == pygame.MOUSEBUTTONUP:
-            cookies += 1
+    screen.fill((4, 146, 194))
+    write(cookies)
+    if enlarge == True:
+        screen.blit(cookie2, (50, 65))
+    else:
+        screen.blit(cookie, (75, 90))
+    pygame.display.update()
+    clock.tick(FPS)
 
 
